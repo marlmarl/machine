2848455a26b180f30e09f10b41243be824030064
marl <marlenape@gmail.com>
2017-09-25 20:09:38 +0200

    slider and swipe plugin

--- js/machine.js
+++ js/machine.js
@@ -7,10 +7,6 @@ $(document).ready(function() {
          render();
       }
       clicked = 1;
-   });
-
-   $('.valve').click(function(e) {
-
       e.preventDefault();
       var info = $(this).find('.infoRef').attr('title');
       var screenframe = $(this).find('.screenRef').attr('title');
@@ -28,6 +24,7 @@ $(document).ready(function() {
           $('body').addClass('modal-open');
           $(info).show();
           $('.screen-mobile').show();
+
       }else{
           $('.screen-mobile').hide();
             $(screenframe).fadeIn(500).show().addClass('active-screen');
@@ -35,20 +32,14 @@ $(document).ready(function() {
       }
    });
 
-   $('.valve4').click(function(e){
-      bellySlides();
-      mobileSlides();
-   })
-
    $('.close-fact').click(function(e) {
       $('.text-container').fadeOut(500);
       $(this).closest('.single-fact-container').hide();
       $('body').removeClass('modal-open');
    });
 
-    window.onresize = function() {
-      console.log($(window).width())
-        if ($(window).width() < 769)
+    window.onresize = function() {  
+        if ($(window).width() < 768)
             $('.text-container').hide();
         else
             $('.text-container').show();
@@ -57,6 +48,8 @@ $(document).ready(function() {
         $('.part').removeClass( 'rotate' );
         $('.screen-mobile').hide();
     }
+    
+
 
    function load() {
       if (loading) {
@@ -69,37 +62,3 @@ $(document).ready(function() {
 
 
 });
-
-
-function bellySlides() {
-    var allSlides = $('#screen4 .fact4slides');
-    var $activeSlide = allSlides.eq(0);
-    $activeSlide.show();
-    var $next = $activeSlide.next();
-    var timer = setInterval(function() {
-        $next.fadeIn(2000);
-        $activeSlide.hide();
-        $activeSlide = $next;
-        console.log(allSlides.last().index());
-        $next = (allSlides.last().index() == allSlides.index($activeSlide)) ?
-            $next = allSlides.eq(0):$activeSlide.next();
-
-    }, 2000);
-}
-
-
-function mobileSlides() {
-    var allSlides = $('#screen-mobile-4 .fact-screen-mobile4');
-    var $activeSlide = allSlides.eq(0);
-    $activeSlide.show();
-    var $next = $activeSlide.next();
-    var timer = setInterval(function() {
-        $next.fadeIn(2000);
-        $activeSlide.hide();
-        $activeSlide = $next;
-        console.log(allSlides.last().index());
-        $next = (allSlides.last().index() == allSlides.index($activeSlide)) ?
-            $next = allSlides.eq(0):$activeSlide.next();
-
-    }, 2000);
-}
