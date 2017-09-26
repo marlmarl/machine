f9c28b63fe8193050b99fc9287708d04223a23ce
marl <marlmarl@users.noreply.github.com>
2017-09-26 12:41:11 +0200

    cesmetic

--- js/slider.js
+++ js/slider.js
@@ -1,32 +1,32 @@
 
 
-$('.slider').each(function() {              // For every slider
-  var $this   = $(this);                    // Current slider
-  var $group  = $this.find('.slide-viewer'); // Get the slide-group (container)
-  var timeout;                              // Sets gap between auto-sliding
+$('.slider').each(function() {
+  var $this   = $(this);
+  var $group  = $this.find('.slide-viewer');
+  var timeout;     //needed?
 
-  function move(newIndex) {          // Creates the slide from old to new one
-    var animateLeft, slideLeft;      // Declare variables
+  function move(newIndex) {
+    var animateLeft, slideLeft;
 
-    // If it is the current slide / animating do nothing
+
     if ($group.is(':animated') || currentIndex === newIndex) {
       return;
     }
 
-    if (newIndex > currentIndex) {   // If new item > current
-      slideLeft = '100%';            // Sit the new slide to the right
-      animateLeft = '-100%';         // Animate the current group to the left
-    } else {                         // Otherwise
-      slideLeft = '-100%';           // Sit the new slide to the left
-      animateLeft = '100%';          // Animate the current group to the right
+    if (newIndex > currentIndex) {
+      slideLeft = '100%';
+      animateLeft = '-100%';
+    } else {
+      slideLeft = '-100%';
+      animateLeft = '100%';
     }
-      
-      // Position new slide to left (if less) or right (if more) of current
+
+
     $slides.eq(newIndex).css( {left: slideLeft, display: 'block'} );
-    $group.animate( {left: animateLeft}, function() {    // Animate slides and
-      $slides.eq(currentIndex).css( {display: 'none'} ); // Hide previous slide
-      $slides.eq(newIndex).css( {left: 0} ); // Set position of the new item
-      $group.css( {left: 0} );               // Set position of group of slides
+    $group.animate( {left: animateLeft}, function() {
+      $slides.eq(currentIndex).css( {display: 'none'} );
+      $slides.eq(newIndex).css( {left: 0} );
+      $group.css( {left: 0} );
     changeActiveClasses(newIndex);
 
     });
@@ -35,31 +35,31 @@ $('.slider').each(function() {              // For every slider
 
 
   $.each($slides, function(index) {
-    // Create a button element for the button
+
     var $button = $('<button type="button" class="slide-btn">&bull;</button>');
-    if (index === currentIndex) {    // If index is the current item
-      $button.addClass('active');    // Add the active class
+    if (index === currentIndex) {
+      $button.addClass('active');
     }
-    $button.on('click', function() { // Create event handler for the button
-      move(index);                   // It calls the move() function
-    }).appendTo('.slide-buttons');   // Add to the buttons holder
-    buttonArray.push($button);       // Add it to the button array
+    $button.on('click', function() {
+      move(index);
+    }).appendTo('.slide-buttons');
+    buttonArray.push($button);
   });
 
     $('.single-fact-container').swipe({
         swipe:function(event, direction, distance, duration, fingerCount){
             if (direction === "left"){
-                if (currentIndex < ($slides.length - 1)) { // If slide < total slides
-                    move(currentIndex + 1);            // Move to next slide
-                } else {                             // Otherwise
-                    move(0);                           // Move to the first slide
+                if (currentIndex < ($slides.length - 1)) {
+                    move(currentIndex + 1);
+                } else {
+                    move(0);
                 }
             }
             if (direction === "right"){
-                if (currentIndex > 0) { // If slide < total slides
-                    move(currentIndex - 1);            // Move to next slide
-                } else {                             // Otherwise
-                    move($slides.length - 1);                           // Move to the first slide
+                if (currentIndex > 0) {
+                    move(currentIndex - 1);
+                } else {
+                    move($slides.length - 1);                         
                 }
             }
         },
